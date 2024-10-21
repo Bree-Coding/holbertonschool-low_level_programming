f487c78eb0668fd4e9d2cbbb79dd3b7c6275220f
Bree-Coding <brendapollagba@gmail.com>
2024-10-21 14:44:28 +0200

    fix 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle,
  * followed by a new line.
- * Return: Always = 0
+ * 
+ * Return: Always = 0.
  */
 
 int main() 
 {
 	puts ("Programming is like building a multilingual puzzle);
-	return (0);
+	Return (0)
 }
