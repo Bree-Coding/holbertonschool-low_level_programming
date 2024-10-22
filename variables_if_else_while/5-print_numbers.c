4bf46ba5ba851d21f3a0ca34b39a9a63fc5637a4
Bree-Coding <brendapollagba@gmail.com>
2024-10-22 20:08:41 +0200

    add 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - Print nubers from zero to ten.
+ *  retun: 0
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 0; x < 10; ++x)
+{
+	putchar(x);
+}
+	putchar('\n');
+	return (0);
+}
