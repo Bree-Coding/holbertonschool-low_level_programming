e667fd8b82e00afd56d5f3a8838f6abeb8239bbe
Bree-Coding <brendapollagba@gmail.com>
2024-10-22 16:08:12 +0200

    add 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase
+ *Return: 0
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++);
+	putchar(ch);
+
+	return (0);
+
+}
