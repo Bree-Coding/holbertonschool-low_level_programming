ccf43c8d1175093eac2cfd45390e3783d8019e61
Bree-Coding <brendapollagba@gmail.com>
2024-10-25 11:21:25 +0200

    fix 4TH

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - Check for c in in lower and upercase.
- * @c: letter c in lower or uppercase
- * Return: 0
+ * _isalpha - Check for letter in in lower and upercase.
+ * @c: letter in lower or uppercase
+ * Return: 1 if it is a letter or else 0
  */
 
 int _isalpha(int c)
 {
 
-	if ((c <= 65 && c >= 90) || (c <= 97 && c >= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
