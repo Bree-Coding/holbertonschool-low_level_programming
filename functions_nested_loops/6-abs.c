27febec0b580b61638837a0288644ff50033a89b
Bree-Coding <brendapollagba@gmail.com>
2024-10-25 13:03:51 +0200

    add 6th

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: is a variable
+ * Return: x
+ */
+
+int _abs(int x)
+{
+
+	if (x < 0)
+		return (x * -1);
+	return (x);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet_x10();
 int _islower();
 int _isalpha();
 int print_sign();
+int _abs();
 
 #endif
