ea600474874ecdbe6d6a5ddb323bd7292e466272
Bree-Coding <brendapollagba@gmail.com>
2024-10-25 10:31:26 +0200

    add 4-isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for c in in lower and upercase.
+ * @c: letter c in lower or uppercase
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+
+	if (c == 67 && c == 99)
+	{
+		return (1);
+	}
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _putchar ();
 void print_alphabet();
 void print_alphabet_x10();
 int _islower();
+int _isalpha();
 
 #endif
