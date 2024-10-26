9f3851d67826eb0be2790a623176890a6cf79523
Bree-Coding <brendapollagba@gmail.com>
2024-10-26 05:16:59 +0200

    add 9-time-table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,30 +11,30 @@ void times_table(void)
 	int r;
 
 	for (x = 0; x <= 9; x++)
-	{
 		for (y = 0; y <= 9; y++)
-
-		{
-			r = x * y;
-
-			if (r < 10)
+		{	r = x * y;
+			if (y < 9)
 				{
-				_putchar(' ');
+				if (r < 10)
+				{	_putchar(' ');
+					_putchar(' ');
+					_putchar((r % 10) + '0'); }
+				else
+				{	_putchar(' ');
+					_putchar((r / 10) + '0');
+					_putchar((r % 10) + '0'); }
+					_putchar(',');
+				}
+			else if (r < 10)
+			{	_putchar(' ');
 				_putchar(' ');
 				_putchar((r % 10) + '0');
-				_putchar(',');
-				}
+				_putchar('\n'); }
 			else
-				{
+			{
 				_putchar(' ');
 				_putchar((r / 10) + '0');
 				_putchar((r % 10) + '0');
-				_putchar(',');
-				}
-		if (y == 9)
-			{
-			_putchar('\n');
-			}
+				_putchar('\n'); }
 		}
-	}
 }
