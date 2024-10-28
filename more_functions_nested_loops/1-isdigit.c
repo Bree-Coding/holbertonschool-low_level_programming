9cf8418221cadcaf153ae9c09241b309b32fa50d
Bree-Coding <brendapollagba@gmail.com>
2024-10-28 10:22:39 +0100

    add 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Return 1 if c is an uppercase
+ * @c: a varriable between 48 and 57
+ * Return: 0, 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{       return (1); }
+
+	else
+	{       return (0); }
+
+}
