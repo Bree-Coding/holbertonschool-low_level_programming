1d86a2a8ad79c825517b7db426acd71bfde32a9f
Bree-Coding <brendapollagba@gmail.com>
2024-10-28 10:59:29 +0100

    add 3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	{
+	for (x = 48; x <= 57; x++)
+
+		_putchar(x);
+	}
+	_putchar('\n');
+}
