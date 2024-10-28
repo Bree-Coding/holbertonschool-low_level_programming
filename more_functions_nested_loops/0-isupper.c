55d86a2ef8f657a506eb071d029db92229a8ca53
Bree-Coding <brendapollagba@gmail.com>
2024-10-28 10:15:23 +0100

    add 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Return 1 if c is an uppercase
+ * @c: a varriable between 65 and 90
+ * Return: 0, 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{	return (1); }
+
+	else
+	{	return (0); }
+
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+
+int _putchar ();
+int _isupper();
+int _isdigit();
+int mul();
+void print_numbers();
+void print_most_numbers();
+void more_numbers();
+void print_line();
+void print_diagonal();
+void print_square();
+void print_triangle();
+
+#endif
