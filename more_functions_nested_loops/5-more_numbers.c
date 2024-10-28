2986ce2ce691caaeb4395994db2629c864a60ae7
Bree-Coding <brendapollagba@gmail.com>
2024-10-28 14:32:49 +0100

    add 5th and 6th task

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,7 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			if (x < 10)
-			{	_putchar((x % 10) + '0'); }
-			else
+			if (x > 9)
 			{	_putchar((x / 10) + '0');
 				_putchar((x % 10) + '0'); }
 		}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: the beginning of the line
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n ; x++)
+		{
+			_putchar('_'); 
+		}
+		_putchar('\n');
+	}
+	else
+	{	
+		_putchar('\n'); 
+	}
+}
