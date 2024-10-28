7eff648b8fc51c66e52474b0934cbcfe25b8dd44
Bree-Coding <brendapollagba@gmail.com>
2024-10-28 14:40:15 +0100

    fix 5th and 6th task

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -13,12 +13,12 @@ void print_line(int n)
 	{
 		for (x = 0; x < n ; x++)
 		{
-			_putchar('_'); 
+			_putchar('_');
 		}
 		_putchar('\n');
 	}
 	else
-	{	
-		_putchar('\n'); 
+	{
+		_putchar('\n');
 	}
 }
