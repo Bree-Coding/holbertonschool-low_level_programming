c28895fc960bd975dd0c9a9133d87d1b1ffa9025
Bree-Coding <brendapollagba@gmail.com>
2024-10-29 00:27:12 +0100

    add directiry

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifdef main_h
+#define main_h
+
+int _putchar ();
+void reset_to_98();
+void swap_int();
+int _strlen();
+void _puts();
+void _puts();
+void rev_string();
+void puts2();
+void puts_half();
+void print_array();
+char *_strcpy();
+int _atoi();
+
+#endif
