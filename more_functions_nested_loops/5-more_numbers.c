70116727560dbd500e626e296b8fa3e36ab70fcf
Bree-Coding <brendapollagba@gmail.com>
2024-10-28 12:52:30 +0100

    aad 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int x;
+
+	n = 0;
+	for (n = 0; n < 10; n++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x < 10)
+			{	_putchar((x % 10) + '0'); }
+			else
+			{	_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0'); }
+		}
+		_putchar('\n');
+	}
+
+}
