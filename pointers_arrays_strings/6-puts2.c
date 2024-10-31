135e7abe151654e7e29308b863e3074bc12e5928
Bree-Coding <brendapollagba@gmail.com>
2024-10-31 12:24:26 +0100

    add 6-puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one another charater
+ *@str: a pointor
+ */
+
+void puts2(char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		x = x + 2;
+	}
+	_putchar('\n');
+}
