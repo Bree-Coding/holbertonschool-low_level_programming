b7a00938d248eff9e123a3837ab12792096de568
Bree-Coding <brendapollagba@gmail.com>
2024-10-31 12:41:32 +0100

    add 6-puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,17 @@
 void puts2(char *str)
 {
 	int x = 0;
+	int y;
 
 	while (str[x] != '\0')
 	{
-		_putchar(str[x]);
-		x = x + 2;
+		x++;
+	}
+	x--;
+
+	for (y = 0; y <= x; y = y + 2)
+	{
+		_putchar(str[y]);
 	}
 	_putchar('\n');
 }
