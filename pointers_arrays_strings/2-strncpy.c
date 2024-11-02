2b25478e7c7cd67041f3c9a712eafcc254a80311
Bree-Coding <brendapollagba@gmail.com>
2024-11-02 23:42:47 +0100

    add 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Writes a function that copies a string
+ * @src: the source string
+ * @dest: the destination string
+ * @n: a varriable length
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (x < n && src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	if (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
+	return (dest);
+}
