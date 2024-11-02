0ebe7ea65aad7e104faf756955732b736935c201
Bree-Coding <brendapollagba@gmail.com>
2024-11-02 21:23:15 +0100

    add main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,11 +6,13 @@ void reset_to_98();
 void swap_int();
 int _strlen();
 void _puts();
+void print_rev(char *s);
 void rev_string();
 void puts2();
 void puts_half();
 void print_array();
 char *_strcpy();
 int _atoi();
+char *_strcat();
 
 #endif
