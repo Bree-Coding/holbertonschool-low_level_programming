e91c649052fad8b155aea75c286c086b2c2d0787
Bree-Coding <brendapollagba@gmail.com>
2024-11-06 09:34:48 +0100

    add new directory recursion

---
+++ recursion/main.h
@@ -0,0 +1,13 @@
+
+#ifndef main_h
+#define main_h
+
+void _puts_recursion();
+void _print_rev_recursion();
+int _strlen_recursion();
+int factorial();
+int _pow_recursion();
+int _sqrt_recursion();
+int is_prime_number();
+
+#endif
