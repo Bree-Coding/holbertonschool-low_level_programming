f82ad87138a6533b339b66589625ced915b63af7
Bree-Coding <brendapollagba@gmail.com>
2024-11-08 19:25:24 +0100

    add work for review

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - A function that returns the value of x
+ * raised to the power of y
+ * @n: an integer
+ * Return: -1, n, 
+ */
+
+int square(int n, int x)
+{
+	square = ((n + n / x) / 2);
+	return (square (x + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	if (n < 2)
+	{	
+		return (n);
+	}
+	if (square == 0)
+	{
+	return (n);
+	}
+}
