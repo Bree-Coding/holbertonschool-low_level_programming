7f928ecd10e1f7bf9205caa91f515407dcb36126
Bree-Coding <brendapollagba@gmail.com>
2024-11-11 14:56:28 +0100

    add directory malloc_free

---
+++ malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef main_h
+#define main_h
+
+#include <stdlib.h>
+
+int _putchar ();
+char *create_array();
+char *_strdup();
+char *str_concat();
+int **alloc_grid();
+void free_grid();
+
+#endif
