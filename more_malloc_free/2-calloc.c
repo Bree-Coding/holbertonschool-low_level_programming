f57137108dc27f8415656dd446fa2001a91da9db
Bree-Coding <brendapollagba@gmail.com>
2024-11-13 22:46:57 +0100

    fix 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	array = malloc(nmemb * size);
 
-	for (x = 0; x < nmemb; x++)
+	for (x = 0; x <= nmemb; x++)
 	{
 		array[x] = 0;
 		return (array);
