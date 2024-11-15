798efa3c2d7e14473f1a4cc06eb5136dc53fe02f
Bree-Coding <brendapollagba@gmail.com>
2024-11-15 18:16:35 +0100

    trying again to fix 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -25,7 +25,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 			printf("Age: %1f\n", d->age);
-			printf("Name: %s\n", d->name);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
