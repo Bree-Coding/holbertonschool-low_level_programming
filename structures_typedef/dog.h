f57dffedf4c5e63f86ec25fdabfae1fd6df52c20
Bree-Coding <brendapollagba@gmail.com>
2024-11-15 18:46:43 +0100

    fix dog.h after new type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,9 +6,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * struct dog - a new type for dog's informations
  * @name: name of the dog
@@ -21,6 +18,9 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
