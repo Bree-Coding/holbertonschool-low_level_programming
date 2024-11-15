1f95c07ce201d595ff3c7dd1ab26ebfbdec18640
Bree-Coding <brendapollagba@gmail.com>
2024-11-15 13:16:13 +0100

    fix 2-print_doc.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,18 +7,25 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		printf("nil");
-	}
-	else if (d->name == NULL || d->age <= 0 || d->owner == NULL)
-	{
-		printf("nil");
-	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %1f\n", d->age);
-		printf("Name: %s\n", d->name);
+		if (d->name == NULL)
+		{
+			printf("Name: nil");
+		}
+		else if (d->age <= 0)
+		{
+			printf("Age: nil");
+		}
+		else if (d->owner == NULL)
+		{
+			printf("Owner: nil");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %1f\n", d->age);
+			printf("Name: %s\n", d->name);
+		}
 	}
 }
