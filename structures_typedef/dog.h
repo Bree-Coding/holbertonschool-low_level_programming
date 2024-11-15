deb15ad94d540fbbdbf416afd04a127519ed86ba
Bree-Coding <brendapollagba@gmail.com>
2024-11-15 10:46:02 +0100

    add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - a new type for dog's informations
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
