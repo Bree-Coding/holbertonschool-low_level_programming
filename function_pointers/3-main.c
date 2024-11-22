86c8f134172d4d28f2890c8d8ba054b13c487cf7
Bree-Coding <brendapollagba@gmail.com>
2024-11-22 22:24:19 +0100

    add 3 for review

---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+
+/**
+ * main - Prints the result of an operation
+ * @argc: the number of arguments
+ * @argv: an array of arguments
+ * Return: result, 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	char oper;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	oper = *argv[2];
+
+	if (oper != '+' || oper != '-' || oper != '*' || oper != '/' || oper != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((oper == '/' || oper == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(argv[2])(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
