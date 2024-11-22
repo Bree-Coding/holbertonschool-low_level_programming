38751ad1546013b6ccf2628fd5e089bc432b6fe4
Bree-Coding <brendapollagba@gmail.com>
2024-11-22 22:25:41 +0100

    add 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ *op_add - Function that returns the result of a simple addition
+ * @a: an variable integer
+ * @b: another variable integer
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that returns the result of a simple substration
+ * @a: an variable integer
+ * @b: another variable integer
+ * Return: a + b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - Function that returns the result of a simple multiplication
+ * @a: an variable integer
+ * @b: another variable integer
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Function that returns the result of a simple division
+ * @a: an variable integer
+ * @b: another variable integer
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Function that returns the remainder of a division
+ * @a: an variable integer
+ * @b: another variable integer
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
