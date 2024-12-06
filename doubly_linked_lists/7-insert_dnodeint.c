f44da487a4c401ad0ce36c0f0b80dc33b7bf30b7
Bree-Coding <brendapollagba@gmail.com>
2024-12-06 21:45:03 +0100

    trying to fix task 7 + betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Function that inserts a new node at a given position
+ * insert_dnodeint_at_index - Function that inserts a new node at
+ * a given position
  * @h: the head of the list
  * @idx: the index of the node
  * @n: a pointer to the number of elements
@@ -19,11 +20,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	for (x = 0; x < idx; x++)
 	{
+		(*h) = (*h)->next;
 		if (*h == NULL)
 		{
 			return (NULL);
 		}
-		(*h) = (*h)->next;
 	}
 
 	new_node = malloc(sizeof(dlistint_t));
@@ -36,7 +37,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = (*h);
 	new_node->next = (*h)->next;
-	
 	(*h)->next->prev = new_node;
 	(*h)->next = new_node;
 	return (new_node);
